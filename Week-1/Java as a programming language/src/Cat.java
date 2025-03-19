9a84a803795eec8f930674cc7a7bbb659ce80b4e
Hussein <alprstigehussein@gmail.com>
2025-03-20 00:11:02 +0200

    This File is Done

--- Week-1/Java as a programming language/src/Cat.java
+++ Week-1/Java as a programming language/src/Cat.java
@@ -1,2 +1,23 @@
 public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public void meow() {
+        System.out.println("The cat named " + name + " says: Meow!");
+    }
+
+    public static void main(String[] args) {
+        // Create instances of the Cat class with names
+        Cat cat1 = new Cat("Whiskers");
+        Cat cat2 = new Cat("Rex");
+
+        // Call the meow method on the cat instances
+        cat1.meow();
+        cat1.meow();
+        cat2.meow();
+        cat1.meow();
+    }
 }
